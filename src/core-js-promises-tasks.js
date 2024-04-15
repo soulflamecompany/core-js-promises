c12533b7845f1cd79e6a0664207e857aef17c805
Filipp Kadushkin <kadushkin.feniks@gmail.com>
2024-04-16 01:15:17 +0300

    feat: implement all tasks +7/-0

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -140,8 +140,25 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let result = '';
+  let chain = Promise.resolve();
+
+  promises.forEach((promise) => {
+    chain = chain.then((value) => {
+      if (value !== undefined) {
+        result += value;
+      }
+      return promise;
+    });
+  });
+
+  return chain.then((value) => {
+    if (value !== undefined) {
+      result += value;
+    }
+    return result;
+  });
 }
 
 module.exports = {
